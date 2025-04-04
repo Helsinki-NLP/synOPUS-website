333fedcdac57dfa993091908c4507a18dbd1d9b6
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2025-04-04 14:00:20 +0300

    header

--- public_html/header.php
+++ public_html/header.php
@@ -1,6 +1,19 @@
 <table><tr>
 <td><a href="index.php">Home</a>&nbsp;/&nbsp;<a href="https://opus.nlpl.eu/legacy/trac">Wiki</a>&nbsp;&nbsp;&nbsp;</td>
 <td>
-      [<a href="Europarl.php">Europarl</a>]
+<?php
+if ($handle = opendir('.')) {
+    while ($entry = readdir($handle)) {
+      if (str_ends_with($entry,'.php'){
+        if (is_link($entry){
+	  echo "[<a href='$entry'>";
+	  echo basename($entry);
+	  echo "</a>] ";
+	}
+      }
+    }
+    closedir($handle);
+}
+?>
 </td></tr></table>
 <br/>
